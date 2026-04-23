f6ff6fe2486cb57974843b139e54702ae18e4e43
amank630 <amank630@gmail.com>
2026-04-23 23:03:15 +0530

    Use Swagger on Live for testing

--- Program.cs
+++ Program.cs
@@ -78,11 +78,13 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
